a2cc627477336fd66047f9ae3819aba572473ffc
codeShinobi-sarthak <codeshinobi.sarthak@gmail.com>
2024-07-14 09:25:09 +0530

    Day_2 completed

---
+++ Day_2.js
@@ -0,0 +1,41 @@
+//! link :  https://courses.chaicode.com/learn/home/30-days-of-Javascript-challenge/30-days-javascript-challenge/section/515627/lesson/3197073
+
+console.log("--Activity-1--"); //* Activity-1
+const num_1 = 23;
+const num_2 = 3;
+
+console.log(num_1 + num_2);
+console.log(num_1 - num_2);
+console.log(num_1 * num_2);
+console.log(num_1 / num_2);
+console.log(num_1 % num_2);
+
+console.log("--Activity-2--"); //* Activity-2
+let num = 5;
+console.log(num);
+num += 1;
+console.log(num);
+num -= 1;
+console.log(num);
+
+console.log("--Activity-3--"); //* Activity-3
+console.log(num_1 > num_2);
+console.log(num_1 < num_2);
+
+console.log(num_1 >= num_2);
+console.log(num_1 <= num_2);
+
+console.log(num_1 == num_2);
+console.log(num_1 === num_2);
+
+console.log("--Activity-4--"); //* Activity-4
+let bool_1 = true;
+let bool_2 = false;
+
+console.log(bool_1 && !bool_2);
+console.log(bool_1 || bool_2);
+console.log(!bool_1);
+
+console.log("--Activity-5--"); //* Activity-5
+let number = 25;
+number > 20 ? console.log("number is greater than 20"): console.log("number is not greater than 20");
