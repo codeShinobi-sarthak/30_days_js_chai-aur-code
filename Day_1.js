84d547d4665e02b334f03fe0844f80f1b1d67e9a
codeShinobi-sarthak <codeshinobi.sarthak@gmail.com>
2024-07-14 08:30:54 +0530

    Add link to resource

--- Day_1.js
+++ Day_1.js
@@ -1,3 +1,5 @@
+//! link: https://courses.chaicode.com/learn/home/30-days-of-Javascript-challenge/30-days-javascript-challenge/section/515627/lesson/3196994
+
 //* Activity-1
 var day = 1;
 console.log(day);
