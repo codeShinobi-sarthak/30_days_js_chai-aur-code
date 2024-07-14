c72b005bc13e8ac1a16fd9f9395dfc518e3c8c2f
codeShinobi-sarthak <codeshinobi.sarthak@gmail.com>
2024-07-14 08:11:14 +0530

    day_1 of js

---
+++ Day_1.js
@@ -0,0 +1,35 @@
+//* Activity-1
+var day = 1;
+console.log(day);
+let language = "js";
+console.log(language);
+
+//* Activity-2
+const boolean = true;
+console.log(boolean);
+
+//* Activity-3
+var num = 5;
+var str = "it's a string";
+var bool = true;
+var arr = [1, 2, 3];
+var obj = {
+  name: "sarthak",
+  age: 20,
+};
+
+console.log(num);
+console.log(str);
+console.log(bool);
+console.log(arr);
+console.log(obj);
+
+//* Activity-4
+let reAssign = "initial";
+console.log(reAssign);
+reAssign = "final";
+console.log(reAssign);
+
+//* Activity-5
+const observeError = "observe the error while reassigning";
+// observeError = "observed"; //! TypeError: Assignment to constant variable.
